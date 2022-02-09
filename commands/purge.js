d017c0ec6ca4d93ee5bb31575ffea9ccac22d1cc
Rayen Attia <rayenattia@gmail.com>
2022-02-10 00:15:52 +0100

    cleanup

--- commands/purge.js
+++ commands/purge.js
@@ -2,22 +2,39 @@ const { ownerId } = require('../config.json');
 
 exports.run = async (bot, message, args) => {
     if ((!message.member.roles.cache.has('937645148563709962'))&&(!message.member.roles.cache.has('937644405706354708'))&&(message.author.id !== ownerId)){
-        return message.channel.send(":x: You don't have the persimmions to use this command!");
+        let msg = await message.channel.send(":x: You don't have the permission to use this command!");
+        setTimeout(() => {
+            msg.delete();
+        }, 2000);
+        return ;
     }
+
     var amount = parseInt(args[0]);
 
-    if (!amount)
-        return message.channel.send('Please specify the amount of messages you want me to delete');
-    if (amount > 100 || amount < 1)
-        return message.channel.send('Please select a number *between* 100 and 1');
+    if (!amount){
+        let msg = await message.channel.send(":exclamation: Please specify the amount of messages you want me to delete.");
+        setTimeout(() => {
+            msg.delete();
+        }, 2000);
+        return ;
+    }
+
+    if (amount > 100 || amount < 1){
+        let msg = await message.channel.send(":exclamation: Please specify an amount between 1 and 100.");
+        setTimeout(() => {
+            msg.delete();
+        }, 2000);
+        return ;
+    }
+
 
     message.channel.bulkDelete(amount).catch((err) => {
         message.channel.send(
-            ':x: Due to Discord Limitations, I cannot delete messages older than 14 days'
+            ':x: Due to Discord Limitations, I cannot delete messages older than 14 days.'
         );
     });
 
-    let msg = await message.channel.send(`Deleted \`${amount}\` messages`);
+    let msg = await message.channel.send(`:white_check_mark: Deleted \`${amount}\` messages!`);
     setTimeout(() => {
         msg.delete();
     }, 2000);
--- commands/send.js
+++ commands/send.js
@@ -1,29 +1,47 @@
 const { ownerId } = require('../config.json');
 
 exports.run = async (bot, message, args) => {
-    console.log(args);
     if (
         !message.member.roles.cache.has('937645148563709962') &&
         !message.member.roles.cache.has('937644405706354708') &&
         message.author.id !== ownerId
     ) {
-        return message.channel.send(":x: You don't have the persimmions to use this command!");
+        let msg = await message.channel.send(":x: You don't have the permission to use this command!");
+        setTimeout(() => {
+            msg.delete();
+        }, 2000);
+        return ;
     }
-    var toSay = args[0];
-    var destination = message.guild.channels.cache.find((channel) => channel.name === args[1]);
 
-    if (!toSay) return message.channel.send('Please specify the message you want to send');
-    if (!destination)
-        return message.channel.send('Please specify the channel you want to send the message to');
+    lastArg = args[args.length - 1];
+    args.pop();
+    var toSay = args.join(' ');
+    var destination = message.guild.channels.cache.find((channel) => channel.name === lastArg);
+
+    if (!toSay){
+        let msg = await message.channel.send(':exclamation: Please specify the message you want to send.');
+        setTimeout(() => {
+            msg.delete();
+        }, 2000);
+        return ;
+    } 
+
+    if (!destination){
+        let msg = await message.channel.send(':exclamation: Please specify a valid channel.');
+        setTimeout(() => {
+            msg.delete();
+        }, 2000);
+        return ;
+    }
 
     message.guild.channels.cache
         .get(destination.id)
         .send(toSay)
         .catch((err) => {
-            message.channel.send(':x: Error sending message');
+            message.channel.send(':x: Error sending message.');
         });
 
-    let msg = await message.channel.send(`Message sent!`);
+    let msg = await message.channel.send(`:white_check_mark: Message sent!`);
     setTimeout(() => {
         msg.delete();
     }, 2000);
