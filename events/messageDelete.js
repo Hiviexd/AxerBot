6213ac0d312dd1ecb412384a236f6d9c469d2490
Rayen Attia <rayenattia@gmail.com>
2022-02-16 10:30:48 +0100

    wasteland slight cleanup

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -2,16 +2,14 @@ module.exports = {
     name: 'messageDelete',
     execute(message, client) {
         //console.log(`a message saying "${message.cleanContent}" was deleted from channel: ${message.channel.name} at ${new Date()}`);
+        if (message.author.bot) return;
+        if (message.channel.type === 'dm') return;
         client.channels.cache.find(c => c.name === "wasteland").send(
             `:x: Deleted from <#${message.channel.id}>
             
-            **${message.author.tag}**:
-            "${message.cleanContent}" 
-
-        Time: ${new Date()}`
+        **${message.author.tag}**:
+        "${message.cleanContent}"`
         );
-            //TODO: make date less cluttered
             //TODO: preferably have all of these in an embed
       }
-    
 }
