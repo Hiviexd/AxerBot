f76a0837ccd26e0a1beacce6d1245357e852b426
Rayen Attia <rayenattia@gmail.com>
2022-02-16 17:27:36 +0100

    switch to embeds

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,15 +1,19 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
     name: 'messageDelete',
     execute(message, client) {
-        //console.log(`a message saying "${message.cleanContent}" was deleted from channel: ${message.channel.name} at ${new Date()}`);
         if (message.author.bot) return;
         if (message.channel.type === 'dm') return;
-        client.channels.cache.find(c => c.name === "wasteland").send(
-            `:x: Deleted from <#${message.channel.id}>
-            
-        **${message.author.tag}**:
-        "${message.cleanContent}"`
-        );
-            //TODO: preferably have all of these in an embed
+
+        const embed = new MessageEmbed()
+            .setColor('#ff5050')
+            .setAuthor(`${message.author.username} deleted a message from #${message.channel.name}`, message.author.displayAvatarURL())
+            .setDescription(`**Message:** ${message.cleanContent}`)
+            .setTimestamp();
+
+            //TODO: figure out a way to send a message and the embed in the same .send()
+            //client.channels.cache.find(c => c.name === "wasteland-test").send(`:x: Deleted from <#${message.channel.id}>`);
+            client.channels.cache.find(c => c.name === "wasteland").send({ embeds: [embed] });
       }
 }
