da10d042fe3d2a89df24a7986f0a9a05b851e67a
Rayen Attia <rayenattia@gmail.com>
2022-02-17 16:26:05 +0100

    log server leaves

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,18 @@
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+    name: 'guildMemberRemove',
+    execute(member) {
+        const memberRoles = member.roles.cache
+        .filter((roles) => roles.id !== member.guild.id)
+        .map((role) => role.toString());
+        //.map(role => role.name);
+        const embed = new MessageEmbed()
+            .setColor('#d97f36')
+            .setAuthor(`${member.user.username}`, member.user.displayAvatarURL())
+            .setDescription(`:wave: ${member.user} has left the server!\n\n**Roles:** \n${memberRoles}`)
+            .setTimestamp();
+
+        console.log('User ' + member.user.tag + ' has left the server!');
+        member.guild.channels.cache.find(c => c.name === "wasteland-test").send({ embeds: [embed] })
+    }
+}
