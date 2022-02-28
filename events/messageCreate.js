8c986566b719fd3ef3d69ece1c3ffcef8a37078e
Rayen Attia <rayenattia@gmail.com>
2022-02-28 11:50:47 +0100

    respond to pings

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -22,13 +22,12 @@ async function processFile() {
 
 module.exports = {
     name: 'messageCreate', 
-    execute(message) {
+    execute(message, bot) {
         //Check if author is a bot or the message was sent in dms and return
-        if (message.author.bot) return;
+        if (message.author === bot.user) return;
         if (message.channel.type === 'dm') return;
         //toUpperCase makes the keyword case insensitive
-        if (message.content.toUpperCase().includes("AXER") === true) {
-            //let quotes = [`gm`, `ඞ`, `what do you want`, `I need to meet an Argentine girl so I can groom her`, `CHILDREN!!!!1!!`, `Connection terminated. I'm sorry to interrupt you, ${message.author.username}, if you still even remember that name, But I'm afraid you've been misinformed. You are not here to receive a badge, nor have you been called here by the individual you assume, although, you have indeed been called.`];
+        if ((message.content.toUpperCase().includes("AXER")) || (message.mentions.has(bot.user))) {
             processFile().then(function(data) {
                 let quotes = data;
                 let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
