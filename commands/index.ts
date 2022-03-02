a0513060dcb5c7b10cce9cc59378044bfe902b11
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-03-02 12:29:12 -0300

    Refactory participant.js in TS

--- commands/index.ts
+++ commands/index.ts
@@ -1,14 +1,15 @@
 import ping from "./misc/ping";
-import beatmap from "./misc/beatmap";
 import mapper from "./user/mapper";
 import choose from "./fun/choose";
 import yesno from "./fun/yesno";
+import participant from "./moderation/participant";
 
 const commands: any = {
 	ping: ping,
 	mapper: mapper,
 	choose: choose,
 	yesno: yesno,
+	participant: participant,
 };
 
 export default commands;
