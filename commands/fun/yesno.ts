28f304b930ec0622dc38a38b9b32dfc9dfa06367
Rayen Attia <rayenattia@gmail.com>
2022-03-07 19:21:34 +0100

    refactor randomMessage + add priv server wordlist

--- commands/fun/yesno.ts
+++ commands/fun/yesno.ts
@@ -1,19 +1,19 @@
 import { Client, Message } from "discord.js";
-import processFile from "../../utils/messages/randomMessage";
+import randomMessage from "../../utils/messages/randomMessage";
 export default {
 	name: "yesno",
-	description: "Yes or no? I can help you.",
+	description: "Yes or no? I can help you decide!",
 	syntax: "!yesno `option`",
 	example: "!yesno `axer cringe?`",
 	category: "fun",
 	run: (bot: Client, message: Message, args: string[]) => {
+		let source: string = "./data/yesno.txt";
+        let privateState: number = 0;
 		if (args.length < 1) {
 			message.channel.send("waht is your question you dumbass");
 			return;
 		}
-		processFile("./data/yesno.txt").then(function (data) {
-			let quotes = data;
-			let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+		randomMessage(source, privateState).then((randomQuote) => {
 			message.channel.send(randomQuote);
 		});
 	},
