041646d5734f271c97ef1a597d502f665c73ecc9
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-03-07 16:02:36 -0300

    Add more emojis to getEmoji function

--- utils/messages/getEmoji.ts
+++ utils/messages/getEmoji.ts
@@ -6,6 +6,10 @@ export default (emoji: string) => {
 		loved: "<:loved:950465283770945547>",
 		qualified: "<:qualified:950465359763361913>",
 		ranked: "<:ranked:950465283917762590>",
+		osu: "<:osu:950459762682781696>",
+		mania: "<:mania:950459762577903616> <:catch:950459762590486568>",
+		taiko: "<:taiko:950459762229788733>",
+		catch: "<:catch:950459762590486568>",
 	};
 
 	const request = emojis[emoji];
