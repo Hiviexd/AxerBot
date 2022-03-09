91ddaae0ca7f3a5d9d11c9814546db094191dde8
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-03-09 16:03:38 -0300

    Prevent some crashes on event handler

--- events/guildMemberRemove.ts
+++ events/guildMemberRemove.ts
@@ -4,40 +4,46 @@ import { consoleLog } from "../utils/core/logger";
 export default {
 	name: "guildMemberRemove",
 	execute(bot: Client) {
-		bot.on("guildMemberRemove", (member) => {
-			if (
-				!member.guild.channels.cache.find((c) => c.name === "wasteland")
-			)
-				return;
-
-			if (!member.user) return;
-
-			const memberRoles = member.roles.cache
-				.filter((roles) => roles.id !== member.guild.id)
-				.map((role) => role.toString());
-			const embed = new MessageEmbed()
-				.setColor("#d97f36")
-				.setAuthor(
-					`${member.user.username}`,
-					member.user.displayAvatarURL()
+		try {
+			bot.on("guildMemberRemove", (member) => {
+				if (
+					!member.guild.channels.cache.find(
+						(c) => c.name === "wasteland"
+					)
 				)
-				.setDescription(
-					`:wave: ${member.user} has left the server!\n\n**Roles:** \n${memberRoles}`
-				)
-				.setTimestamp();
+					return;
 
-			consoleLog(
-				"guildMemberRemove",
-				`User ${member.user.tag} has left the server!`
-			);
+				if (!member.user) return;
 
-			const channel: any = member.guild.channels.cache
-				.filter((c) => c.name === "wasteland")
-				.first();
+				const memberRoles = member.roles.cache
+					.filter((roles) => roles.id !== member.guild.id)
+					.map((role) => role.toString());
+				const embed = new MessageEmbed()
+					.setColor("#d97f36")
+					.setAuthor(
+						`${member.user.username}`,
+						member.user.displayAvatarURL()
+					)
+					.setDescription(
+						`:wave: ${member.user} has left the server!\n\n**Roles:** \n${memberRoles}`
+					)
+					.setTimestamp();
 
-			if (!channel) return;
+				consoleLog(
+					"guildMemberRemove",
+					`User ${member.user.tag} has left the server!`
+				);
 
-			channel.send({ embeds: [embed] });
-		});
+				const channel: any = member.guild.channels.cache
+					.filter((c) => c.name === "wasteland")
+					.first();
+
+				if (!channel) return;
+
+				channel.send({ embeds: [embed] });
+			});
+		} catch (e: any) {
+			console.error(e);
+		}
 	},
 };
--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -2,6 +2,7 @@ import { Client } from "discord.js";
 import randomMessage from "../utils/messages/randomMessage";
 import osuTimestamp from "../utils/messages/osuTimestamp";
 import * as dotenv from "dotenv";
+import osuURL from "../utils/messages/osuURLmanager";
 dotenv.config();
 const privserver: any = process.env.PRIVATESERVER;
 const privlist: any = process.env.PRIVATELIST;
@@ -15,22 +16,26 @@ export default {
 			if (message.channel.type === "DM") return;
 
 			const bot_user: any = bot.user;
-            let source: string;
-            let privateState: number;
-            //check if keyword or bot are mentioned
-			if (message.content.toUpperCase().includes("AXER") || message.mentions.has(bot_user)) {
+			let source: string;
+			let privateState: number;
+			//check if keyword or bot are mentioned
+			if (
+				message.content.toUpperCase().includes("AXER") ||
+				message.mentions.has(bot_user)
+			) {
 				//check if server uses private list
-                if (message.guildId === privserver) {
-                    source = privlist;
-                    privateState = 1;
-                } else {
-                    source = "./data/axer.txt";
-                    privateState = 0;
-                }
-                randomMessage(source, privateState).then((randomQuote) => {
-                    message.channel.send(randomQuote);
-                });
-            }
+				if (message.guildId === privserver) {
+					source = privlist;
+					privateState = 1;
+				} else {
+					source = "./data/axer.txt";
+					privateState = 0;
+				}
+				randomMessage(source, privateState).then((randomQuote) => {
+					message.channel.send(randomQuote);
+				});
+			}
+			osuURL(message);
 			osuTimestamp(message);
 		});
 	},
