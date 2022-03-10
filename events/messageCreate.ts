0091a2178215cef42a2b968e9be74277413b927e
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-03-10 18:20:38 -0300

    Refactory quotes system

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -2,6 +2,7 @@ import { Client } from "discord.js";
 import randomMessage from "../utils/messages/randomMessage";
 import osuTimestamp from "../utils/messages/osuTimestamp";
 import * as dotenv from "dotenv";
+import * as database from "./../database";
 // import osuURL from "../utils/messages/osuURLmanager";
 dotenv.config();
 const privserver: any = process.env.PRIVATESERVER;
@@ -10,33 +11,50 @@ const privlist: any = process.env.PRIVATELIST;
 export default {
 	name: "messageCreate",
 	execute(bot: Client) {
-		bot.on("messageCreate", (message) => {
-			//Check if author is a bot or the message was sent in dms and return
+		bot.on("messageCreate", async (message) => {
 			if (message.author === bot.user) return;
 			if (message.channel.type === "DM") return;
-
 			const bot_user: any = bot.user;
-			let source: string;
-			let privateState: number;
-			//check if keyword or bot are mentioned
+			const guild_db = await database.guilds.findById(message.guildId);
+
 			if (
 				message.content.toUpperCase().includes("AXER") ||
 				message.mentions.has(bot_user)
 			) {
-				//check if server uses private list
-				if (message.guildId === privserver) {
-					source = privlist;
-					privateState = 1;
+				if (!guild_db.fun.enable) return;
+
+				if (guild_db.fun.mode == "default") {
+					let source: string;
+					let privateState: number;
+
+					if (message.guildId === privserver) {
+						source = privlist;
+						privateState = 1;
+					} else {
+						source = "./data/axer.txt";
+						privateState = 0;
+					}
+					randomMessage(source, privateState).then((randomQuote) => {
+						message.channel.send(randomQuote);
+					});
 				} else {
-					source = "./data/axer.txt";
-					privateState = 0;
+					if (!guild_db.fun.enable) return;
+
+					const random_quote = message.channel.messages.cache
+						.filter(
+							(q) =>
+								q.author.id != bot_user.id &&
+								!q.content.toUpperCase().includes("AXER") &&
+								q.mentions.users.size == 0
+						)
+						.random().content;
+
+					message.channel.send(random_quote);
 				}
-				randomMessage(source, privateState).then((randomQuote) => {
-					message.channel.send(randomQuote);
-				});
+				// osuURL(message);
+			} else {
+				return osuTimestamp(message);
 			}
-			// osuURL(message);
-			osuTimestamp(message);
 		});
 	},
 };
