d9bcbc5426d2a154bdf838d5eccaf9a594046328
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-03-13 15:50:21 -0300

    Prevent some crashes

--- events/messageDelete.ts
+++ events/messageDelete.ts
@@ -33,6 +33,8 @@ export default {
 					.filter((c) => c.name === "wasteland")
 					.first();
 
+				if (!channel) return;
+
 				channel.send({ embeds: [embed] });
 			});
 		} catch (e: any) {
--- events/messageUpdate.ts
+++ events/messageUpdate.ts
@@ -53,6 +53,8 @@ export default {
 					.filter((c) => c.name == "wasteland")
 					.first();
 
+				if (!channel) return;
+
 				channel.send({ embeds: [embed] });
 			});
 		} catch (e: any) {
