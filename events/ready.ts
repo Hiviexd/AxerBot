76ed859b9e9a1c50c1a1db4ea7fd53967714d31a
Rayen Attia <rayenattia@gmail.com>
2022-03-22 22:18:21 +0100

    cleanups

--- events/ready.ts
+++ events/ready.ts
@@ -15,7 +15,7 @@ export default {
 
 			// Set the Presence of the bot user
 			bot_user.setPresence({
-				activities: [{ name: "with your mother" }],
+				activities: [{ name: "ax!help | ax!setprefix" }],
 			});
 		});
 	},
