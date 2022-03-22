7200c10ffb340cdcf0fef282190b7c00548ecf39
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-03-22 15:50:44 -0300

    Cooldown system pt2

--- utils/core/commandHandler.ts
+++ utils/core/commandHandler.ts
@@ -17,13 +17,6 @@ export default async function commandHandler(bot: Client, message: Message) {
 
 	if (!message.content.startsWith(guild.prefix)) return;
 
-	message.channel.sendTyping().catch((e) => {
-		consoleError(
-			"commandHandler",
-			"Can't sent typing status on ".concat(message.channel.id)
-		);
-	});
-
 	const args = message.content
 		.slice(guild.prefix.length, message.content.length)
 		.split(" ");
