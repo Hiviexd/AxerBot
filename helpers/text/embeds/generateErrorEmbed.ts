a6ec4367e4b68e1efb5c5581f73425a6a8d1df05
Rayen Attia <rayenattia@gmail.com>
2022-06-02 19:02:35 +0100

    create error embed generator

---
+++ helpers/text/embeds/generateErrorEmbed.ts
@@ -0,0 +1,19 @@
+/*
+    * ================ generateErrorEmbed.ts
+    ? Generates an error embed
+	? @param message - The message that triggered the command.
+	? @param response - The response to be displayed (Optional).
+	? @returns The success embed.
+*/
+
+import {MessageEmbed } from "discord.js";
+
+export default(response?: string): MessageEmbed => {
+	return new MessageEmbed({
+		title: "Error",
+		description: response || ":x: There was an error executing this operation.",
+		color: "#ff5050",
+	});
+};
+
+				
