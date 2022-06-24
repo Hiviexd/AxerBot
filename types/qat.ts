a12f9b8f803c7154bcea89a1b38302ec70d46066
Rayen Attia <rayenattia@gmail.com>
2022-06-24 06:01:07 +0100

    initial typing

--- types/qat.ts
+++ types/qat.ts
@@ -1,7 +1,7 @@
 import { GameModeName } from "./game_mode";
 import { Timestamp } from "./timestamp";
 
-export type EventType = "rank" | "disqualify" | "qualify" | "nominate" | "love";
+export type EventType = "nominate" | "qualify" | "disqualify" | "nomination_reset";
 
 export interface InterOpEvent {
 	modes: GameModeName[];
@@ -27,4 +27,179 @@ export interface InterOpEvent {
 export interface InterOpEventResponse {
 	status: number;
 	data: InterOpEvent[];
+}
+
+export interface BeatmapEvent {
+	modes: GameModeName[];
+	_id: string;
+	timestamp: Timestamp;
+	beatmapsetId: number;
+	creatorId: number;
+	creatorName: string;
+	artistTitle: string;
+	id: string;
+}
+
+export interface Nomination {
+	_id: string;
+	type: EventType;
+	timestamp: Timestamp;
+	betmapsetId: number;
+	creatorId: number;
+	creatorName: string;
+	modes: GameModeName[];
+	discussionId: number | null;
+	userId: number;
+	content: string | null;
+	genre: string;
+	language: string;
+}
+
+export interface NominationReset {
+	_id: string;
+	type: EventType;
+	timestamp: Timestamp;
+	beatmapsetId: number;
+	creatorId: number;
+	creatorName: string;
+	modes: GameModeName[];
+	discussionId: number | null;
+	userId: number;
+	content: string | null;
+	genre: string;
+	language: string;
+	__v?: number;
+	createdAt?: Timestamp;
+	isBnOrNat?: boolean;
+	isReviewed?: boolean;
+	isUnique?: boolean;
+	responsibleNominators?: [];
+	updatedAt?: Timestamp;
+	obviousness?: number | null;
+	severity?: number | null;
+}
+
+export interface QACheck {
+	_id: string;
+	user: string;
+	event: BeatmapEvent;
+	timestamp: Timestamp;
+	mode: string;
+	createdAt: Timestamp;
+	updatedAt: Timestamp;
+	__v: number;
+	comment?: string;
+	id: string;
+}
+
+export interface DisqualifiedQACheck {
+	modes: GameModeName[];
+	obviousness: number | null;
+	severity: number | null;
+	isBnOrNat: boolean;
+	isUnique: boolean;
+	responsibleNominators: [];
+	isReviewed: boolean;
+	_id: string;
+	type: EventType;
+	timestamp: Timestamp;
+	beatmapsetId: number;
+	creatorId: number;
+	creatorName: string;
+	discussionId: number | null;
+	userId: number | null;
+	artistTitle: string;
+	content: string | null;
+	genre: string;
+	language: string;
+	__v: number;
+	createdAt: Timestamp;
+	updatedAt: Timestamp;
+	qaComment?: string | null;
+	id: string;
+}
+
+export interface UserActivityResponse {
+	status: number;
+	data: string | {
+		uniqueNominations: Nomination[];
+		NominationsDisqualified: NominationReset[]; // ? dqs recieved
+		nominationsPopped: NominationReset[]; // ? pops recieved
+		disqualifications: NominationReset[]; // ? dqs done by user
+		pops: NominationReset[]; // ? pops done by user
+		qualityAssuranceChecks: QACheck[];
+		disqualifiedQualityAssuranceChecks: DisqualifiedQACheck[];
+	}
+}
+
+export interface QatUserModesInfo {
+	mode: string;
+	level: string;
+}
+
+export interface QatUserHistory {
+	group: string;
+	date: Timestamp;
+	mode: string;
+	kind: string;
+	relatedEvaluation?: string | null;
+}
+
+export interface QatUser {
+	groups: string[];
+	isVetoMediator: boolean;
+	isBnEvaluator: boolean;
+	inBag: boolean;
+	isTrialNat: boolean;
+	bnProfileBadge: number;
+	natProfileBadge: number;
+	rankedBeatmapsets: number;
+	requestStatus: string[];
+	genrePreferences: string[];
+	genreNegativePreferences: string[];
+	languagePreferences: string[];
+	languageNegativePreferences: string[];
+	osuStylePreferences: string[];
+	osuStyleNegativePreferences: string[];
+	taikoStylePreferences: string[];
+	taikoStyleNegativePreferences: string[];
+	catchStylePreferences: string[];
+	catchStyleNegativePreferences: string[];
+	maniaStylePreferences: string[];
+	maniaStyleNegativePreferences: string[];
+	maniaKeymodePreferences: string[];
+	maniaKeymodeNegativePreferences: string[];
+	detailPreferences: string[];
+	detailNegativePreferences: string[];
+	mapperPreferences: string[];
+	mapperNegativePreferences: string[];
+	isBnFinderAnonymous: boolean;
+	_id: string;
+	osuId: number;
+	username: string;
+	createdAt: Timestamp;
+	updatedAt: Timestamp;
+	__v: number;
+	modesInfo: QatUserModesInfo[];
+	history: QatUserHistory[];
+	isFeatureTester: boolean;
+	requestLink: string;
+	stylePreferences: string[];
+	isNat: boolean;
+	isBn: boolean;
+	isBnOrNat: boolean;
+	hasBasicAccess: boolean;
+	hasFullReadAccess: boolean;
+	isNatOrTrialNat: boolean;
+	modes: string[];
+	fullModes: string[];
+	probationModes: string[];
+	bnDuration: number;
+	natDuration: number;
+	id: string;
+}
+
+export interface QatUserResponse {
+	status: number;
+	data: QatUser;
 }
