43045e298c2d337bf55851b2f36eb83288044f12
Rayen Attia <rayenattia@gmail.com>
2022-06-24 11:17:34 +0100

    db migration

---
+++ database/migrations/addBNsiteToCooldown.ts
@@ -0,0 +1,22 @@
+import { guilds } from ".."
+import { consoleLog, consoleCheck } from "../../helpers/core/logger";
+
+export default async () => {
+    consoleLog("addBNsiteToCooldowns", "Checking all guilds")
+
+    const allGuilds = await guilds.find()
+    allGuilds.forEach(async (guild:any) => {
+        if (!guild.cooldowns.BNsite) {
+            guild.cooldowns.BNsite = {
+                size: 0,
+				ends_at: {},
+				current_increments: 0,
+				increments: 0,
+				channels: [],
+            }
+            await guilds.findByIdAndUpdate(guild._id, guild)
+        }
+    });
+    
+    consoleCheck("addBNsiteToCooldowns", "All guilds updated!")
+}
