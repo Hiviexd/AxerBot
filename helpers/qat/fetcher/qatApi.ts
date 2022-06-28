5b1c35ed4f16cbc85a412e9deaae027c5c916b4a
Rayen Attia <rayenattia@gmail.com>
2022-06-28 22:37:39 +0100

    api call to get all qat users

--- helpers/qat/fetcher/qatApi.ts
+++ helpers/qat/fetcher/qatApi.ts
@@ -1,9 +1,11 @@
 import { userActivity } from "./qatUserActivity";
 import { user } from "./qatUser";
+import { allUsers } from "./qatAllUsers";
 
 export default {
     fetch: {
         userActivity: userActivity,
         user: user,
+        allUsers: allUsers,
     },
 };
