f4aeae65eb752d09451b6e2b8df89094259cac81
Paulo Ricardo <62405013+Sebola3461@users.noreply.github.com>
2022-09-09 13:52:00 -0300

    Fix modhub embeds filter

--- helpers/modhub/url/processModhubURL.ts
+++ helpers/modhub/url/processModhubURL.ts
@@ -1,6 +1,27 @@
 import { Message } from "discord.js";
+import { guilds } from "../../../database";
 import RequestEmbed from "../../../responses/modhub/RequestEmbed";
 
-export default (url: URL, type: string, message: Message) => {
-	if (type == "request") RequestEmbed(url, message);
+export default async (url: URL, type: string, message: Message) => {
+	const guild = await guilds.findById(message.guildId);
+
+	if (!guild) return;
+
+	if (type == "request" && checkEmbed("modhub", message))
+		RequestEmbed(url, message);
+
+	function checkEmbed(category: string, message: Message) {
+		if (!guild) return true;
+
+		if (message.member?.permissions.has("MANAGE_MESSAGES", true))
+			return true;
+
+		if (
+			(guild.embeds[category].all && !guild.embeds[category].none) ||
+			guild.embeds[category].channels.includes(message.channelId)
+		)
+			return true;
+
+		return false;
+	}
 };
