58b05695932b0f30e70cde433ce41909042e5e07
Hivie <rayenattia@gmail.com>
2022-10-01 17:15:52 +0100

    switch to cache for server count

--- modules/presence/presence.ts
+++ modules/presence/presence.ts
@@ -1,11 +1,10 @@
 import { Client } from "discord.js";
 
 export default async function presence(bot: Client, bot_user: any, ready: boolean) {
-    const guilds = await bot.guilds.fetch();
     ready ?
         bot_user.setPresence({
             status: "online",
-            activities: [{ name: `${guilds.size} servers | -help`, type: "WATCHING" }],
+            activities: [{ name: `${bot.guilds.cache.size} servers | -help`, type: "WATCHING" }],
         })
     :
         bot_user.setPresence({
