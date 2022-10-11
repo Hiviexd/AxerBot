444bba64108ce011bb8afbc1a13046ad06c6dda6
Alexander <sashakostyunin1@gmail.com>
2022-10-11 21:14:12 +0300

    Fix game mode IDs

--- types/game_mode.ts
+++ types/game_mode.ts
@@ -4,14 +4,14 @@
  * https://osu.ppy.sh/docs/index.html#gamemode
  */
 export enum GameMode {
-	/** osu!catch */
-	fruits = 2,
-	/** osu!mania */
-	mania = 1,
 	/** osu!standard */
 	osu = 0,
 	/** osu!taiko */
-	taiko = 3,
+	taiko = 1,
+	/** osu!catch */
+	fruits = 2,
+	/** osu!mania */
+	mania = 3,
 }
 
 export type GameModeName = "osu" | "taiko" | "mania" | "fruits";
