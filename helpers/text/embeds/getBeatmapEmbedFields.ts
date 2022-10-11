74a2a7dac4dd6d2e993ab912bedf18bfe0de9250
Alexander <sashakostyunin1@gmail.com>
2022-10-11 21:15:18 +0300

    Update beatmap embed field value to work with latest rework changes

--- helpers/text/embeds/getBeatmapEmbedFields.ts
+++ helpers/text/embeds/getBeatmapEmbedFields.ts
@@ -1,8 +1,7 @@
-import { CatchRuleset } from "osu-catch-stable";
-import { ManiaRuleset } from "osu-mania-stable";
-import { BeatmapDecoder } from "osu-parsers";
-import { StandardRuleset } from "osu-standard-stable";
-import { TaikoRuleset } from "osu-taiko-stable";
+import { DifficultyAttributes, IBeatmap } from "osu-classes";
+import { StandardDifficultyAttributes } from "osu-standard-stable";
+import { TaikoDifficultyAttributes } from "osu-taiko-stable";
+import { CatchDifficultyAttributes } from "osu-catch-stable";
 import { Beatmap } from "../../../types/beatmap";
 import { GameModeName } from "../../../types/game_mode";
 import timeString from "../timeString";
@@ -10,133 +9,118 @@ import timeString from "../timeString";
 export default (
 	beatmap: Beatmap,
 	mode: GameModeName,
-	beatmap_file: string,
-	mods?: string
+	parsed: IBeatmap,
+	attributes: DifficultyAttributes,
 ) => {
-	mods ? mods : (mods = "NM");
-	let description = "";
+	const bpmMin = parsed.bpmMin.toFixed(0);
+	const bpmMax = parsed.bpmMax.toFixed(0);
+	const bpmMode = parsed.bpmMode.toFixed(0);
+	const bpm = bpmMin === bpmMax 
+		? bpmMode 
+		: `(Min: ${bpmMin} / Max: ${bpmMax})`;
+
+	const description = [
+		`SR: \`${beatmap.difficulty_rating.toFixed(2)}\``,
+		`Combo: \`${attributes.maxCombo}\``,
+		`Length: \`${timeString(beatmap.total_length)}\``,
+		`BPM: \`${bpm}\``,
+	];
 
 	switch (mode) {
 		case "osu": {
-			const decoder = new BeatmapDecoder();
-			const parsed: any = decoder.decodeFromString(beatmap_file);
-			const ruleset = new StandardRuleset();
-			const difficultyCalculator =
-				ruleset.createDifficultyCalculator(parsed);
-			const attributes = difficultyCalculator.calculateWithMods(
-				ruleset.createModCombination(mods)
+			const difficulty = attributes as StandardDifficultyAttributes;
+
+			description.push(
+				`OD: \`${difficulty.overallDifficulty.toFixed(1)}\``
 			);
 
-			description = `SR: \`${beatmap.difficulty_rating.toFixed(
-				2
-			)}\` Combo: \`${attributes.maxCombo}\` Length: \`${timeString(
-				beatmap.total_length
-			)}\` BPM: \`${
-				parsed.bpmMin == parsed.bpmMax
-					? parsed.bpmMin.toFixed(0)
-					: `(Min: ${parsed.bpmMin.toFixed(0)} / Max: ${Number(
-							parsed.bpmMax
-					  ).toFixed(0)})`
-			}\`
-            OD: \`${attributes.overallDifficulty.toFixed(
-				1
-			)}\` HP: \`${attributes.drainRate.toFixed(
-				1
-			)}\` AR: \`${attributes.approachRate.toFixed(1)}\` CS: \`${
-				beatmap.cs
-			}\` Aim Strain: \`${attributes.aimStrain.toFixed(
-				2
-			)}\` Speed Strain: \`${attributes.speedStrain.toFixed(2)}\``;
+			description.push(
+				`HP: \`${difficulty.drainRate.toFixed(1)}\``)
+			; 
+
+			description.push(
+				`AR: \`${difficulty.approachRate.toFixed(1)}\``)
+			; 
+
+			description.push(
+				`CS: \`${parsed.difficulty.circleSize.toFixed(1)}\``
+			);
+
+			description.push(
+				`Aim Strain: \`${difficulty.aimDifficulty.toFixed(2)}\``
+			);
+
+			description.push(
+				`Speed Strain: \`${difficulty.speedDifficulty.toFixed(2)}\``
+				);
 
 			break;
 		}
 
 		case "taiko": {
-			const decoder = new BeatmapDecoder();
-			const parsed: any = decoder.decodeFromString(beatmap_file);
-			const ruleset = new TaikoRuleset();
-			const difficultyCalculator =
-				ruleset.createDifficultyCalculator(parsed);
-			const attributes = difficultyCalculator.calculateWithMods(
-				ruleset.createModCombination(mods)
+			const difficulty = attributes as TaikoDifficultyAttributes;
+
+			description.push(
+				`OD: \`${parsed.difficulty.overallDifficulty.toFixed(1)}\``
 			);
-			description = `SR: \`${beatmap.difficulty_rating.toFixed(
-				2
-			)}\` Combo: \`${attributes.maxCombo}\` Length: \`${timeString(
-				beatmap.total_length
-			)}\` BPM: \`${
-				parsed.bpmMin == parsed.bpmMax
-					? parsed.bpmMin.toFixed(0)
-					: `(Min: ${parsed.bpmMin.toFixed(0)} / Max: ${Number(
-							parsed.bpmMax
-					  ).toFixed(0)})`
-			}\`
-            OD: \`${beatmap.accuracy}\` HP: \`${
-				beatmap.drain
-			}\` Stamina Strain: \`${attributes.staminaStrain.toFixed(2)}\` 
-            Colour Strain: \`${attributes.colourStrain.toFixed(
-				2
-			)}\` Rhythm Strain: \`${attributes.rhythmStrain.toFixed(2)}\``;
+
+			description.push(
+				`HP: \`${parsed.difficulty.drainRate.toFixed(1)}\``
+			); 
+
+			description.push(
+				`Stamina Strain: \`${difficulty.staminaDifficulty.toFixed(2)}\``
+			);
+
+			description.push(
+				`Colour Strain: \`${difficulty.colourDifficulty.toFixed(2)}\``
+			);
+
+			description.push(
+				`Rhythm Strain: \`${difficulty.rhythmDifficulty.toFixed(2)}\``
+			);
+
+			description.push(
+				`Peaks Strain: \`${difficulty.peakDifficulty.toFixed(2)}\``
+			);
 
 			break;
 		}
 
 		case "fruits": {
-			const decoder = new BeatmapDecoder();
-			const parsed: any = decoder.decodeFromString(beatmap_file);
-			const ruleset = new CatchRuleset();
-			const difficultyCalculator =
-				ruleset.createDifficultyCalculator(parsed);
-			const attributes = difficultyCalculator.calculateWithMods(
-				ruleset.createModCombination(mods)
+			const difficulty = attributes as CatchDifficultyAttributes;
+
+			description.push(
+				`OD: \`${parsed.difficulty.overallDifficulty.toFixed(1)}\``
 			);
 
-			description = `SR: \`${beatmap.difficulty_rating.toFixed(
-				2
-			)}\` Combo: \`${attributes.maxCombo}\` Length: \`${timeString(
-				beatmap.total_length
-			)}\` BPM: \`${
-				parsed.bpmMin == parsed.bpmMax
-					? parsed.bpmMin.toFixed(0)
-					: `(Min: ${parsed.bpmMin.toFixed(0)} / Max: ${Number(
-							parsed.bpmMax
-					  ).toFixed(0)})`
-			}\`
-            OD: \`${beatmap.accuracy}\` HP: \`${
-				beatmap.drain
-			}\` AR: \`${attributes.approachRate.toFixed(1)}\` CS: \`${
-				beatmap.cs
-			}\``;
+			description.push(
+				`HP: \`${parsed.difficulty.drainRate.toFixed(1)}\``
+			); 
+
+			description.push(
+				`AR: \`${difficulty.approachRate.toFixed(1)}\``
+			); 
+
+			description.push(
+				`CS: \`${parsed.difficulty.circleSize.toFixed(1)}\``
+			);
 
 			break;
 		}
 
 		case "mania": {
-			const decoder = new BeatmapDecoder();
-			const parsed: any = decoder.decodeFromString(beatmap_file);
-			const ruleset = new ManiaRuleset();
-			const difficultyCalculator =
-				ruleset.createDifficultyCalculator(parsed);
-			const attributes = difficultyCalculator.calculateWithMods(
-				ruleset.createModCombination(mods)
+			description.push(
+				`OD: \`${parsed.difficulty.overallDifficulty.toFixed(1)}\``
 			);
 
-			description = `SR: \`${beatmap.difficulty_rating.toFixed(
-				2
-			)}\` Combo: \`${attributes.maxCombo}\` Length: \`${timeString(
-				beatmap.total_length
-			)}\` BPM: \`${
-				parsed.bpmMin == parsed.bpmMax
-					? parsed.bpmMin.toFixed(0)
-					: `(Min: ${parsed.bpmMin.toFixed(0)} / Max: ${Number(
-							parsed.bpmMax
-					  ).toFixed(0)})`
-			}\`
-            OD: \`${beatmap.accuracy}\` HP: \`${beatmap.drain}\``;
+			description.push(
+				`HP: \`${parsed.difficulty.drainRate.toFixed(1)}\``
+			);
 
 			break;
 		}
 	}
 
-	return description;
+	return description.join(' ');
 };
