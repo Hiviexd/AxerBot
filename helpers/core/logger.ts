d4eead788fa21cf6039451d9c0823fcd9b75aa8b
Hivie <rayenattia@gmail.com>
2022-11-30 13:58:00 +0100

    add time to logger

--- helpers/core/logger.ts
+++ helpers/core/logger.ts
@@ -1,15 +1,22 @@
 import "colors";
 
 export function consoleLog(module_name: string, message: string) {
-	console.log(`[${module_name}]`.bgYellow.black.concat(message.bgBlue.black));
+	console.log(
+		`${new Date().toLocaleTimeString()}`.bgCyan.black +
+			`[${module_name}]`.bgYellow.black.concat(message.bgBlue.black)
+	);
 }
 
 export function consoleError(module_name: string, message: string) {
-	console.log(`[${module_name}]`.bgYellow.black.concat(message.bgRed.black));
+	console.log(
+		`${new Date().toLocaleTimeString()}`.bgCyan.black +
+			`[${module_name}]`.bgYellow.black.concat(message.bgRed.black)
+	);
 }
 
 export function consoleCheck(module_name: string, message: string) {
 	console.log(
-		`[${module_name}]`.bgYellow.black.concat(message.bgGreen.black)
+		`${new Date().toLocaleTimeString()}`.bgCyan.black +
+			`[${module_name}]`.bgYellow.black.concat(message.bgGreen.black)
 	);
 }
