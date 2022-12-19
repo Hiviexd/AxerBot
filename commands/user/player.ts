32dbf221164d4b5e337551b9aa780ae9b64bba66
Hivie <rayenattia@gmail.com>
2022-12-19 13:31:18 +0100

    disable usermention cuz broken for now

--- commands/user/player.ts
+++ commands/user/player.ts
@@ -26,12 +26,12 @@ export default {
 				type: 3,
 				max_value: 1,
 			},
-			{
+			/*{
 				name: "usermention",
 				description: "By user mention (This doesn't ping the user)",
 				type: 6,
 				max_value: 1,
-			},
+			},*/
 			{
 				name: "mode",
 				description: "Gamemode info to view",
