1bbc1f8348040f7dd3752e342776a961c3a68806
Sebola3461 <mixvideostubecontato@gmail.com>
2023-02-08 19:40:37 -0300

    Add first time qat run

--- modules/tracking/qatTracking.ts
+++ modules/tracking/qatTracking.ts
@@ -1,8 +1,8 @@
 import {
-	Client,
-	MessageActionRow,
-	MessageButton,
-	MessageEmbed,
+    Client,
+    MessageActionRow,
+    MessageButton,
+    MessageEmbed,
 } from "discord.js";
 import { tracks } from "../../database";
 import { readFileSync, writeFileSync } from "fs";
@@ -14,208 +14,215 @@ import getEmoji from "../../helpers/text/getEmoji";
 import colors from "../../constants/colors";
 
 async function qatTracking(bot: Client) {
-	const allTracks = await tracks.find({ type: "qat" });
+    const allTracks = await tracks.find({ type: "qat" });
 
-	let storedData = JSON.parse(
-		readFileSync(
-			path.resolve(__dirname + "/../../cache/nominators.json"),
-			"utf8"
-		)
-	);
+    let storedData = JSON.parse(
+        readFileSync(
+            path.resolve(__dirname + "/../../cache/nominators.json"),
+            "utf8"
+        )
+    );
 
-	const liveData = await qatApi.fetch.allUsers();
-	const differentData: QatUser[] = [];
+    const liveData = await qatApi.fetch.allUsers();
+    const differentData: QatUser[] = [];
 
-	// dickwads and people who don't want to be tracked go here
-	const blacklistedBNs = [9487458, 10959501, 33599, 3558897];
+    if (storedData.length == 0)
+        return writeFileSync(
+            path.resolve(__dirname + "/../../cache/nominators.json"),
+            JSON.stringify(liveData),
+            "utf8"
+        );
 
-	if (storedData.length != 0) {
-		for (const user of liveData.data) {
-			const storedDataIndex = storedData.findIndex(
-				(u: QatUser) => u._id == user._id
-			);
+    // dickwads and people who don't want to be tracked go here
+    const blacklistedBNs = [9487458, 10959501, 33599, 3558897];
 
-			if (
-				storedData[storedDataIndex] &&
-				storedData[storedDataIndex].requestStatus.includes("closed") !=
-					user.requestStatus.includes("closed")
-			)
-				differentData.push(user);
-		}
-	}
+    if (storedData.length != 0) {
+        for (const user of liveData.data) {
+            const storedDataIndex = storedData.findIndex(
+                (u: QatUser) => u._id == user._id
+            );
 
-	for (const track of allTracks) {
-		await mapBns(track);
-	}
+            if (
+                storedData[storedDataIndex] &&
+                storedData[storedDataIndex].requestStatus.includes("closed") !=
+                    user.requestStatus.includes("closed")
+            )
+                differentData.push(user);
+        }
+    }
 
-	async function mapBns(track: any) {
-		let usersToSend: QatUser[] = [];
+    for (const track of allTracks) {
+        await mapBns(track);
+    }
 
-		async function checkModes(user: QatUser) {
-			let allow = false;
+    async function mapBns(track: any) {
+        let usersToSend: QatUser[] = [];
 
-			for (const mode of user.modes) {
-				if (track.targets.modes.includes(mode)) allow = true;
-			}
+        async function checkModes(user: QatUser) {
+            let allow = false;
 
-			return allow;
-		}
+            for (const mode of user.modes) {
+                if (track.targets.modes.includes(mode)) allow = true;
+            }
 
-		for (const user of differentData) {
-			if (blacklistedBNs.includes(user.osuId)) continue;
+            return allow;
+        }
 
-			if (await checkModes(user)) {
-				usersToSend.push(user);
-			}
-		}
+        for (const user of differentData) {
+            if (blacklistedBNs.includes(user.osuId)) continue;
 
-		for (const user of usersToSend) {
-			await sendUpdate(user, track);
-		}
-	}
+            if (await checkModes(user)) {
+                usersToSend.push(user);
+            }
+        }
 
-	async function sendUpdate(bn: QatUser, track: any) {
-		const footer = {
-			text: "BN website",
-			iconURL: "https://bn.mappersguild.com/images/qatlogo.png",
-		};
-		const modeIcons = bn.modes
-			.map((mode: string) => {
-				return `${getEmoji(mode)} `;
-			})
-			.join("")
-			.trim();
+        for (const user of usersToSend) {
+            await sendUpdate(user, track);
+        }
+    }
 
-		const texts: { [key: string]: any } = {
-			open: {
-				title: `🟢 Open (<t:${Math.trunc(
-					new Date().valueOf() / 1000
-				)}:R>)`,
-				thumbnail: {
-					url: `https://a.ppy.sh/${bn.osuId}`,
-				},
-				color: colors.green,
-				description: `**[${bn.username}](https://osu.ppy.sh/users/${bn.osuId})** ${modeIcons} is now **accepting** BN requests!\n Check out their preferences below:`,
-				fields: [
-					{
-						name: "Positive",
-						value: getBNPreferences.positive(bn),
-						inline: true,
-					},
-					{
-						name: "Negative",
-						value: getBNPreferences.negative(bn),
-						inline: true,
-					},
-				],
-				footer: footer,
-			},
-			closed: {
-				title: `🔴 Closed (<t:${Math.trunc(
-					new Date().valueOf() / 1000
-				)}:R>)`,
-				thumbnail: {
-					url: `https://a.ppy.sh/${bn.osuId}`,
-				},
-				color: colors.red,
-				description: `**[${bn.username}](https://osu.ppy.sh/users/${bn.osuId})** ${modeIcons} is **no longer** accepting BN requests.`,
-				footer: footer,
-			},
-		};
+    async function sendUpdate(bn: QatUser, track: any) {
+        const footer = {
+            text: "BN website",
+            iconURL: "https://bn.mappersguild.com/images/qatlogo.png",
+        };
+        const modeIcons = bn.modes
+            .map((mode: string) => {
+                return `${getEmoji(mode)} `;
+            })
+            .join("")
+            .trim();
 
-		const open = bn.requestStatus.includes("closed") ? false : true;
+        const texts: { [key: string]: any } = {
+            open: {
+                title: `🟢 Open (<t:${Math.trunc(
+                    new Date().valueOf() / 1000
+                )}:R>)`,
+                thumbnail: {
+                    url: `https://a.ppy.sh/${bn.osuId}`,
+                },
+                color: colors.green,
+                description: `**[${bn.username}](https://osu.ppy.sh/users/${bn.osuId})** ${modeIcons} is now **accepting** BN requests!\n Check out their preferences below:`,
+                fields: [
+                    {
+                        name: "Positive",
+                        value: getBNPreferences.positive(bn),
+                        inline: true,
+                    },
+                    {
+                        name: "Negative",
+                        value: getBNPreferences.negative(bn),
+                        inline: true,
+                    },
+                ],
+                footer: footer,
+            },
+            closed: {
+                title: `🔴 Closed (<t:${Math.trunc(
+                    new Date().valueOf() / 1000
+                )}:R>)`,
+                thumbnail: {
+                    url: `https://a.ppy.sh/${bn.osuId}`,
+                },
+                color: colors.red,
+                description: `**[${bn.username}](https://osu.ppy.sh/users/${bn.osuId})** ${modeIcons} is **no longer** accepting BN requests.`,
+                footer: footer,
+            },
+        };
 
-		const embed = new MessageEmbed(texts[open ? "open" : "closed"]);
+        const open = bn.requestStatus.includes("closed") ? false : true;
 
-		const buttons = new MessageActionRow();
-		const buttons2 = new MessageActionRow();
+        const embed = new MessageEmbed(texts[open ? "open" : "closed"]);
 
-		if (!bn.requestStatus.includes("closed")) {
-			const dmButton = new MessageButton();
-			dmButton
-				.setStyle("LINK")
-				.setLabel("Send message (osu!)")
-				.setURL(`https://osu.ppy.sh/home/messages/users/${bn.osuId}`);
+        const buttons = new MessageActionRow();
+        const buttons2 = new MessageActionRow();
 
-			buttons.addComponents(dmButton);
+        if (!bn.requestStatus.includes("closed")) {
+            const dmButton = new MessageButton();
+            dmButton
+                .setStyle("LINK")
+                .setLabel("Send message (osu!)")
+                .setURL(`https://osu.ppy.sh/home/messages/users/${bn.osuId}`);
 
-			if (bn.requestStatus.includes("personalQueue") && bn.requestLink) {
-				const siteName = new URL(bn.requestLink).hostname.split(".")[0];
-				const personalQueueButton = new MessageButton();
-				personalQueueButton
-					.setStyle("LINK")
-					.setLabel(`Personal queue (${siteName})`)
-					.setURL(bn.requestLink);
-				buttons.addComponents(personalQueueButton);
-			}
+            buttons.addComponents(dmButton);
 
-			if (bn.requestStatus.includes("globalQueue")) {
-				const globalQueueButton = new MessageButton();
-				globalQueueButton
-					.setStyle("LINK")
-					.setLabel(`Global queue`)
-					.setURL(`https://bn.mappersguild.com/modrequests`);
-				buttons2.addComponents(globalQueueButton);
-			}
-		}
+            if (bn.requestStatus.includes("personalQueue") && bn.requestLink) {
+                const siteName = new URL(bn.requestLink).hostname.split(".")[0];
+                const personalQueueButton = new MessageButton();
+                personalQueueButton
+                    .setStyle("LINK")
+                    .setLabel(`Personal queue (${siteName})`)
+                    .setURL(bn.requestLink);
+                buttons.addComponents(personalQueueButton);
+            }
 
-		const guild = bot.guilds.cache.get(track.guild);
+            if (bn.requestStatus.includes("globalQueue")) {
+                const globalQueueButton = new MessageButton();
+                globalQueueButton
+                    .setStyle("LINK")
+                    .setLabel(`Global queue`)
+                    .setURL(`https://bn.mappersguild.com/modrequests`);
+                buttons2.addComponents(globalQueueButton);
+            }
+        }
 
-		if (!guild) return;
+        const guild = bot.guilds.cache.get(track.guild);
 
-		const channel = guild.channels.cache.get(track.channel);
+        if (!guild) return;
 
-		// ! deletes trackers upon booting for some reason dont use
-		// if (!channel) return tracks.findByIdAndDelete(track._id);
+        const channel = guild.channels.cache.get(track.channel);
 
-		if (!channel) return;
+        // ! deletes trackers upon booting for some reason dont use
+        // if (!channel) return tracks.findByIdAndDelete(track._id);
 
-		function allowSend() {
-			if (bn.requestStatus.includes("closed") && !track.targets.closed)
-				return false;
+        if (!channel) return;
 
-			if (!bn.requestStatus.includes("closed") && !track.targets.open)
-				return false;
+        function allowSend() {
+            if (bn.requestStatus.includes("closed") && !track.targets.closed)
+                return false;
 
-			return true;
-		}
+            if (!bn.requestStatus.includes("closed") && !track.targets.open)
+                return false;
 
-		if (channel.isText() && allowSend()) {
-			if (!bn.requestStatus.includes("closed")) {
-				return await channel
-					.send({
-						embeds: [embed],
-						components: bn.requestStatus.includes("globalQueue")
-							? [buttons, buttons2]
-							: [buttons],
-					})
-					.catch((e) => {
-						console.error(e);
-					});
-			} else {
-				return await channel
-					.send({
-						embeds: [embed],
-					})
-					.catch((e) => {
-						console.error(e);
-					});
-			}
-		}
+            return true;
+        }
 
-		return void {};
-	}
+        if (channel.isText() && allowSend()) {
+            if (!bn.requestStatus.includes("closed")) {
+                return await channel
+                    .send({
+                        embeds: [embed],
+                        components: bn.requestStatus.includes("globalQueue")
+                            ? [buttons, buttons2]
+                            : [buttons],
+                    })
+                    .catch((e) => {
+                        console.error(e);
+                    });
+            } else {
+                return await channel
+                    .send({
+                        embeds: [embed],
+                    })
+                    .catch((e) => {
+                        console.error(e);
+                    });
+            }
+        }
 
-	writeFileSync(
-		path.resolve(__dirname + "/../../cache/nominators.json"),
-		JSON.stringify(liveData.data),
-		"utf8"
-	);
-	setTimeout(async () => {
-		await qatTracking(bot);
-	}, 30000); // ? checks every 30 seconds
+        return void {};
+    }
 
-	return void {};
+    writeFileSync(
+        path.resolve(__dirname + "/../../cache/nominators.json"),
+        JSON.stringify(liveData.data),
+        "utf8"
+    );
+    setTimeout(async () => {
+        await qatTracking(bot);
+    }, 30000); // ? checks every 30 seconds
+
+    return void {};
 }
 
 export default qatTracking;
