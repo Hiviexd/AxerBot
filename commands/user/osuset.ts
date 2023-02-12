67cfc309bd54265044ad7d06e3902b049f20182e
Sebola3461 <mixvideostubecontato@gmail.com>
2023-02-12 18:39:13 -0300

    Clear osuset

--- commands/user/osuset.ts
+++ commands/user/osuset.ts
@@ -39,6 +39,7 @@ osuset.builder
                     value: "mapper",
                 }
             )
+            .setRequired(true)
     );
 
 osuset.setExecuteFunction(async (command) => {
@@ -53,9 +54,7 @@ osuset.setExecuteFunction(async (command) => {
     if (!user) return command.editReply("Something is wrong... Try again.");
 
     const username = command.options.getString("username", true);
-    const embed = command.options.get("embed", true).value
-        ? command.options.get("embed", true).value?.toString()
-        : "player";
+    const embed = command.options.getString("embed", true);
 
     user.osu.username = username;
     user.osu.embed = embed;
@@ -72,48 +71,3 @@ osuset.setExecuteFunction(async (command) => {
 });
 
 export default osuset;
-
-// export default {
-// 	name: "osuset",
-// 	help: {
-// 		description: "Sets your credentials so the bot recognizes you.",
-// 		syntax: "/osuset `<field>` `<value>`",
-// 		example:
-// 			"/osuset `user` `Hivie`\n /osuset `user` `HEAVENLY MOON`\n /osuset `embed` `mapper`\n /osuset `embed` `player`",
-// 	},
-// 	category: "osu",
-// 	config: {
-// 		type: 1,
-// 		options: [
-// 			{
-// 				name: "username",
-// 				description: "Your osu account username (can have spaces)",
-// 				type: 3,
-// 				max_value: 1,
-// 				required: true,
-// 			},
-// 			{
-// 				name: "embed",
-// 				description:
-// 					"Embed type to display when i detect your account url",
-// 				type: 3,
-// 				max_value: 1,
-// 				required: true,
-// 				choices: [
-// 					{
-// 						name: "Player",
-// 						value: "player",
-// 					},
-// 					{
-// 						name: "Mapper",
-// 						value: "mapper",
-// 					},
-// 				],
-// 			},
-// 		],
-// 	},
-// 	interaction: true,
-// 	run: async (bot: Client, command: ChatInputCommandInteraction) => {
-
-// 	},
-// };
