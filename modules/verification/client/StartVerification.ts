2a725cd89c8e2d6ce5217e776fac10bce41bce1c
Sebola3461 <mixvideostubecontato@gmail.com>
2023-02-15 13:51:14 -0300

    Prevent verification system crashes

--- modules/verification/client/StartVerification.ts
+++ modules/verification/client/StartVerification.ts
@@ -5,6 +5,7 @@ import {
     EmbedBuilder,
     GuildMember,
     ChannelType,
+    PermissionFlagsBits,
 } from "discord.js";
 import { guilds } from "../../../database";
 import parseMessagePlaceholderFromMember from "../../../helpers/text/parseMessagePlaceholderFromMember";
@@ -25,11 +26,19 @@ export default async (member: GuildMember) => {
         .get(member.guild.id)
         ?.channels.cache.get(guild_db.verification.channel);
 
-    if (!verification_channel || verification_channel.type != ChannelType.GuildText) {
+    if (
+        !verification_channel ||
+        verification_channel.type != ChannelType.GuildText ||
+        !member.guild.members.cache
+            .get(member.client.user.id)
+            ?.permissionsIn(verification_channel)
+            .has(PermissionFlagsBits.SendMessages)
+    ) {
         consoleLog(
             "Verification",
             `Verification channel is not set or deleted in ${member.guild.name}`
         );
+
         return member.client.users.cache
             .get(member.guild.ownerId)
             ?.send({
@@ -38,13 +47,36 @@ export default async (member: GuildMember) => {
                         .setTitle("⚠️ Verification system alert")
                         .setColor(colors.yellowBright)
                         .setDescription(
-                            `The verification system in your server \`${member.guild.name}\` is not working properly. It's possible that you didn't set any valid channel or the channel is deleted.
+                            `The verification system in your server \`${member.guild.name}\` is not working properly. It's possible that you didn't set any valid channel, the channel is deleted or i don't have permissions to send messages in that channel.
                             \`${member.user.tag}\` is waiting for verification. Please verify the user manually and fix the system.
                             Reach out to a developer in the [support server](https://discord.gg/MAsnz96qGy) if you need help.`
                         ),
                 ],
-            });
+            })
+            .catch(console.error);
     }
+
+    if (
+        !member.guild.members.cache
+            .get(member.client.user.id)
+            ?.permissionsIn(verification_channel)
+            .has(PermissionFlagsBits.SendMessages)
+    )
+        return member.client.users.cache
+            .get(member.guild.ownerId)
+            ?.send({
+                embeds: [
+                    new EmbedBuilder()
+                        .setTitle("⚠️ Verification system alert")
+                        .setColor(colors.yellowBright)
+                        .setDescription(
+                            `The verification system in your server \`${member.guild.name}\` is not working properly. I don't have permissions to send messages in the channel <#${verification_channel.id}>.
+                    \`${member.user.tag}\` is waiting for verification. Please verify the user manually and fix the system.
+                    Reach out to a developer in the [support server](https://discord.gg/MAsnz96qGy) if you need help.`
+                        ),
+                ],
+            })
+            .catch(console.error);
 
     if (verification.status != 200 || !verification.data) {
         const error = new EmbedBuilder({
@@ -85,7 +117,8 @@ export default async (member: GuildMember) => {
                     "Verification",
                     `Sent verification to ${member.user.tag} in ${member.guild.name}`
                 );
-            });
+            })
+            .catch(console.error);
     } else {
         verification_channel.send({
             content: parseMessagePlaceholderFromMember(
