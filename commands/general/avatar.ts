2660bbd0e476d02a879effe2b21281f46c797405
Sebola3461 <mixvideostubecontato@gmail.com>
2023-02-15 16:23:32 -0300

    Add osu! avatar fetch to /avatar

--- commands/general/avatar.ts
+++ commands/general/avatar.ts
@@ -8,6 +8,8 @@ import {
 import UserNotFound from "../../responses/embeds/UserNotFound";
 import colors from "../../constants/colors";
 import { SlashCommand } from "../../models/commands/SlashCommand";
+import abbreviation from "../../helpers/text/abbreviation";
+import osuApi from "../../helpers/osu/fetcher/osuApi";
 
 const avatar = new SlashCommand(
     "avatar",
@@ -27,14 +29,41 @@ avatar.builder
     )
     .addStringOption((option) =>
         option.setName("id").setDescription("Get avatar by user id")
+    )
+    .addStringOption((option) =>
+        option.setName("osu_username").setDescription("Get user's osu avatar")
     );
 
 avatar.setExecuteFunction(async (command) => {
     // ? prevent errors
 
     let user: User | undefined = undefined;
+    const osuInput = command.options.getString("osu_username");
     const idInput = command.options.getString("id");
     const userInput = command.options.getUser("user");
+
+    if (osuInput) return sendOsuAvatar();
+
+    async function sendOsuAvatar() {
+        if (!osuInput) return;
+
+        const osuUser = await osuApi.fetch.user(osuInput);
+
+        if (!osuUser || !osuUser.data || osuUser.status != 200)
+            return command.editReply({ embeds: [UserNotFound] });
+
+        const avatarEmbed = new EmbedBuilder()
+            .setColor(colors.purple)
+            .setTitle(`${abbreviation(osuInput)} osu! avatar`)
+            .setImage(`https://a.ppy.sh/${osuUser.data.id}`)
+            .setFooter({
+                text: `Requested by ${command.user.tag}`,
+                iconURL: command.user.displayAvatarURL({
+                    extension: "png",
+                }),
+            });
+        command.editReply({ embeds: [avatarEmbed] }).catch(console.error);
+    }
 
     if (idInput) {
         try {
