8c0c096e00985c47215bb1ced71cb10d8335f93f
Sebola3461 <mixvideostubecontato@gmail.com>
2023-02-19 10:48:06 -0300

    Implement a best way to manage reminders

--- commands/general/subcommands/reminder/removeReminder.ts
+++ commands/general/subcommands/reminder/removeReminder.ts
@@ -1,11 +1,12 @@
-/**
- * TODO: Add a confirmation message before deleting reminders
- */
-import { SlashCommandSubcommand } from "../../../../models/commands/SlashCommandSubcommand";
-import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
-import colors from "../../../../constants/colors";
+import { EmbedBuilder, StringSelectMenuBuilder } from "discord.js";
 import * as database from "../../../../database";
+import { generateStepEmbedWithChoices } from "../../../../helpers/commands/generateStepEmbedWithChoices";
 import generateErrorEmbed from "../../../../helpers/text/embeds/generateErrorEmbed";
+import truncateString from "../../../../helpers/text/truncateString";
+import { vanillaRelativeTime } from "../../../../helpers/text/vanillaRelativeDate";
+import { SlashCommandSubcommand } from "../../../../models/commands/SlashCommandSubcommand";
+import generateErrorEmbedWithTitle from "../../../../helpers/text/embeds/generateErrorEmbedWithTitle";
+
 interface IReminder {
     time: number;
     creationTime: number;
@@ -19,17 +20,7 @@ const removeReminder = new SlashCommandSubcommand(
     "remove a reminder"
 );
 
-removeReminder.builder.addIntegerOption((option) =>
-    option
-        .setName("index")
-        .setDescription("Use /reminder list to get a reminder index")
-        .setRequired(true)
-        .setMinValue(0)
-);
-
 removeReminder.setExecuteFunction(async (command) => {
-    const index = command.options.getInteger("index", true);
-
     let user = await database.users.findById(command.user.id);
     if (!user) return;
 
@@ -39,31 +30,64 @@ removeReminder.setExecuteFunction(async (command) => {
             embeds: [generateErrorEmbed("You have no reminders to remove!")],
         });
 
-    if (index > reminders.length || index < 1)
-        return command.editReply({
-            embeds: [
-                generateErrorEmbed(
-                    "That is not a valid reminder index! Use /reminder list to get a reminder index"
-                ),
-            ],
+    const remindersSelectMenu = new StringSelectMenuBuilder()
+        .setPlaceholder("Select a reminder")
+        .setMinValues(1);
+
+    reminders.forEach((r, i) => {
+        remindersSelectMenu.addOptions({
+            label: `#${i + 1} | (${vanillaRelativeTime(
+                new Date(r.time),
+                new Date()
+            )}) ${truncateString(r.message, 25, true)}`,
+            value: i.toString(),
         });
-
-    const reminderMessage = reminders[index - 1].message;
-    reminders.splice(index - 1, 1);
-
-    await database.users.findByIdAndUpdate(command.user.id, {
-        reminders: reminders,
     });
 
-    const embed = new EmbedBuilder()
-        .setTitle("🗑️ Reminder Removed")
-        .setDescription(`Removed reminder at index ${index}`)
-        .addFields({
-            name: "Reminder",
-            value: reminderMessage,
-        });
+    generateStepEmbedWithChoices(
+        command,
+        "📑 Select a reminder to remove",
+        "You can select up to 25 reminders",
+        remindersSelectMenu
+    )
+        .then(async (options) => {
+            const updatedReminders = reminders.filter(
+                (r, i) => !options.data.includes(i.toString())
+            );
 
-    return command.editReply({ embeds: [embed] });
+            await database.users.findByIdAndUpdate(command.user.id, {
+                reminders: updatedReminders,
+            });
+
+            const embed = new EmbedBuilder()
+                .setTitle("🗑️ Reminder Removed")
+                .setDescription(`Removed ${options.data.length} reminder(s)`)
+                .addFields({
+                    name: "Reminders",
+                    value: reminders
+                        .filter((r, i) => options.data.includes(i.toString()))
+                        .map(
+                            (r, i) =>
+                                `**#${i + 1} |** (<t:${Math.trunc(
+                                    r.time / 1000
+                                )}:R>) ${truncateString(r.message, 25, true)}`
+                        )
+                        .join("\n"),
+                });
+
+            return command.editReply({ content: "", embeds: [embed] });
+        })
+        .catch((e) => {
+            return command.editReply({
+                content: "",
+                embeds: [
+                    generateErrorEmbedWithTitle(
+                        "Don't leave me waiting!",
+                        "Please, do the things during the right time."
+                    ),
+                ],
+            });
+        });
 });
 
 export default removeReminder;
