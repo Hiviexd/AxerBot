4a9b7508bf52c010e3c5693285b428f5d24d9e84
Sebola3461 <mixvideostubecontato@gmail.com>
2023-02-20 18:03:09 -0300

    Eh, status sucks

--- helpers/osu/fetcher/startConnection.ts
+++ helpers/osu/fetcher/startConnection.ts
@@ -9,45 +9,46 @@ const osu_client_id = process.env.OSU_CLIENT_ID;
 const osu_client_secret = process.env.OSU_CLIENT_SECRET;
 
 async function listen() {
-	consoleLog("getServerAuthToken", "Refreshing server authorization token");
+    consoleLog("getServerAuthToken", "Refreshing server authorization token");
 
-	let tokens: any = {};
+    let tokens: any = {};
 
-	try {
-		let _t = await axios("https://osu.ppy.sh/oauth/token", {
-			method: "post",
-			timeout: 999999,
-			headers: {
-				"Content-Type": "application/json",
-			},
-			data: {
-				client_id: osu_client_id,
-				client_secret: osu_client_secret,
-				grant_type: "client_credentials",
-				scope: "public",
-			},
-		});
+    try {
+        let _t = await axios("https://osu.ppy.sh/oauth/token", {
+            method: "post",
+            timeout: 999999,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            data: {
+                client_id: osu_client_id,
+                client_secret: osu_client_secret,
+                grant_type: "client_credentials",
+                scope: "public",
+            },
+        });
 
-		tokens = _t.data;
+        tokens = _t.data;
 
-		// Auto-Refresh token
-		setTimeout(listen, Number(tokens.expires_in) * 1000);
+        // Auto-Refresh token
+        setTimeout(listen, Number(tokens.expires_in) * 1000);
 
-		process.env.OSU_API_ACCESS_TOKEN = tokens.access_token;
+        process.env.OSU_API_ACCESS_TOKEN = tokens.access_token;
+        console.log(process.env.OSU_API_ACCESS_TOKEN);
 
-		consoleCheck(
-			"getServerAuthToken",
-			"Server authorization token refreshed"
-		);
+        consoleCheck(
+            "getServerAuthToken",
+            "Server authorization token refreshed"
+        );
 
-		return tokens;
-	} catch (e: any) {
-		consoleError("getServerAuthToken", "Error during token refresh:\n");
-		console.error(e);
+        return tokens;
+    } catch (e: any) {
+        consoleError("getServerAuthToken", "Error during token refresh:\n");
+        console.error(e);
 
-		setTimeout(listen, 5000);
-		return tokens;
-	}
+        setTimeout(listen, 5000);
+        return tokens;
+    }
 }
 
 listen();
