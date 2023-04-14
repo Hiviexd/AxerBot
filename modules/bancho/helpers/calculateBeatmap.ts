4ba8d4a499870b1e6596b69c3b9a2d9a1f18b319
Sebola3461 <mixvideostubecontato@gmail.com>
2023-04-14 15:34:26 -0300

    Save last /np beatmap

--- modules/bancho/helpers/calculateBeatmap.ts
+++ modules/bancho/helpers/calculateBeatmap.ts
@@ -2,22 +2,44 @@ import { PrivateMessage } from "bancho.js";
 
 import { parseOsuBeatmapURL } from "../../../helpers/text/parseOsuBeatmapURL";
 import { sendBeatmapCalculation } from "./sendBeatmapCalculation";
+import { banchoUsers } from "../../../database";
 
 export async function calculateBeatmapFromAction(pm: PrivateMessage) {
-    const action = pm.getAction();
+    try {
+        const action = pm.getAction();
 
-    if (!action) return;
+        if (!action) return;
 
-    const split =
-        /[A-Za-z0-9]+:\/\/([A-Za-z]+(\.[A-Za-z]+)+)\/[A-Za-z0-9]+\/[0-9]+.*\/[0-9]+/i;
-    const args = split.exec(action);
+        const split =
+            /[A-Za-z0-9]+:\/\/([A-Za-z]+(\.[A-Za-z]+)+)\/[A-Za-z0-9]+\/[0-9]+.*\/[0-9]+/i;
+        const args = split.exec(action);
 
-    if (!args) return;
+        if (!args) return;
 
-    const beatmapURL = parseOsuBeatmapURL(args[0]);
+        const beatmapURL = parseOsuBeatmapURL(args[0]);
 
-    if (beatmapURL.error || !beatmapURL.data) return;
-    if (!beatmapURL.data.beatmap_id) return;
+        if (beatmapURL.error || !beatmapURL.data) return;
+        if (!beatmapURL.data.beatmap_id) return;
 
-    sendBeatmapCalculation(pm, beatmapURL.data.beatmap_id);
+        const user = await pm.user.fetchFromAPI();
+
+        await banchoUsers.updateOne(
+            { _id: user.id },
+            {
+                $set: {
+                    last_beatmap: beatmapURL.data.beatmap_id,
+                    last_beatmapset: beatmapURL.data.beatmapset_id,
+                },
+            }
+        );
+
+        sendBeatmapCalculation(pm, beatmapURL.data.beatmap_id);
+    } catch (e: any) {
+        console.error(e);
+        pm.user.sendMessage(
+            `Can't calculate this beatmap! Reason: ${
+                e.message || "unknown error"
+            }`
+        );
+    }
 }
