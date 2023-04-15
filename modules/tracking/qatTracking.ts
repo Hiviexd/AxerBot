8a8f40159ae44508373b4eddc92a7e1ae97223fa
Sebola3461 <mixvideostubecontato@gmail.com>
2023-04-15 01:04:22 -0300

    Send ping to bnsite ws

--- modules/tracking/qatTracking.ts
+++ modules/tracking/qatTracking.ts
@@ -39,6 +39,22 @@ async function qatTracking(bot: Client) {
 
     qatWebsocket.on("message", messageListener);
 
+    setInterval(pingWebsocket, 30000);
+
+    function pingWebsocket() {
+        consoleLog("QatTracking", "Sending ping to bnsite");
+
+        qatWebsocket.ping("PING", false, (error) => {
+            if (error)
+                return consoleError(
+                    "QatTracking",
+                    `Can't ping bnsite! ${error}`
+                );
+
+            consoleCheck("QatTracking", "Ping sent to bnsite!");
+        });
+    }
+
     function closeListener(e: any) {
         console.error(e);
 
