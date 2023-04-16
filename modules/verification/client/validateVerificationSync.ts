bac0e26dc226d919c778c767b34659517bb0c178
Sebola3461 <mixvideostubecontato@gmail.com>
2023-04-16 14:37:35 -0300

    we did a little trolling

--- modules/verification/client/validateVerificationSync.ts
+++ modules/verification/client/validateVerificationSync.ts
@@ -39,7 +39,7 @@ export async function validateVerificationSync(
 
                 if (channel) {
                     channel.send({
-                        content: `<@${member}>`,
+                        content: `${member}`,
                         embeds: [
                             generateSuccessEmbed(
                                 "Your account info is validated!"
