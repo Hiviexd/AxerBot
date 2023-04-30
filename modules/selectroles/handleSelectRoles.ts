e5857a74458dcea0fac88ee598e780018394620a
Sebola3461 <mixvideostubecontato@gmail.com>
2023-04-30 16:48:41 -0300

    Prevent crash

--- modules/selectroles/handleSelectRoles.ts
+++ modules/selectroles/handleSelectRoles.ts
@@ -9,62 +9,71 @@ import generateErrorEmbed from "../../helpers/text/embeds/generateErrorEmbed";
 export async function handleSelectRoles(
     selectMenu: StringSelectMenuInteraction
 ) {
-    const idFields = selectMenu.customId.split(",");
+    try {
+        const idFields = selectMenu.customId.split(",");
 
-    console.log(idFields);
+        console.log(idFields);
 
-    if (idFields[1] != "selectroles" || !selectMenu.member) return;
+        if (idFields[1] != "selectroles" || !selectMenu.member) return;
 
-    selectMenu.deferUpdate();
+        selectMenu.deferUpdate();
 
-    const rolesToManage = selectMenu.values;
+        const rolesToManage = selectMenu.values;
 
-    const managedRoles = [] as { added: boolean; name: string }[];
-    const roleManager = selectMenu.member.roles as GuildMemberRoleManager;
+        const managedRoles = [] as { added: boolean; name: string }[];
+        const roleManager = selectMenu.member.roles as GuildMemberRoleManager;
 
-    for (const roleId of rolesToManage) {
-        try {
-            if (roleManager.cache.has(roleId)) {
-                await roleManager.remove(roleId);
+        for (const roleId of rolesToManage) {
+            try {
+                if (roleManager.cache.has(roleId)) {
+                    await roleManager.remove(roleId);
 
-                managedRoles.push({
-                    added: false,
-                    name:
-                        selectMenu.guild?.roles.cache.get(roleId)?.name ||
-                        "Unknown Role",
-                });
-            } else {
-                await roleManager.add(roleId);
+                    managedRoles.push({
+                        added: false,
+                        name:
+                            selectMenu.guild?.roles.cache.get(roleId)?.name ||
+                            "Unknown Role",
+                    });
+                } else {
+                    await roleManager.add(roleId);
 
-                managedRoles.push({
-                    added: true,
-                    name:
-                        selectMenu.guild?.roles.cache.get(roleId)?.name ||
-                        "Unknown Role",
-                });
+                    managedRoles.push({
+                        added: true,
+                        name:
+                            selectMenu.guild?.roles.cache.get(roleId)?.name ||
+                            "Unknown Role",
+                    });
+                }
+            } catch (error) {
+                console.error(error);
             }
-        } catch (error) {
-            console.error(error);
         }
-    }
 
-    if (managedRoles.length == 0)
-        return selectMenu.followUp({
+        if (managedRoles.length == 0)
+            return selectMenu.followUp({
+                embeds: [generateErrorEmbed("Something went wrong!")],
+                ephemeral: true,
+            });
+
+        const embed = new EmbedBuilder()
+            .setTitle("📝 Roles managed")
+            .setDescription(
+                `\`\`\`diff\n${managedRoles
+                    .map((role) => `${role.added ? "+" : "-"} @${role.name}`)
+                    .join("\n")}\`\`\``
+            )
+            .setColor(colors.pink);
+
+        selectMenu.followUp({
+            embeds: [embed],
+            ephemeral: true,
+        });
+    } catch (error) {
+        console.error(error);
+
+        selectMenu.followUp({
             embeds: [generateErrorEmbed("Something went wrong!")],
             ephemeral: true,
         });
-
-    const embed = new EmbedBuilder()
-        .setTitle("📝 Roles managed")
-        .setDescription(
-            `\`\`\`diff\n${managedRoles
-                .map((role) => `${role.added ? "+" : "-"} @${role.name}`)
-                .join("\n")}\`\`\``
-        )
-        .setColor(colors.pink);
-
-    selectMenu.followUp({
-        embeds: [embed],
-        ephemeral: true,
-    });
+    }
 }
