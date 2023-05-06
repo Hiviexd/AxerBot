76187b893d4da34591748c2bddd137485f9c4945
Hivie <rayenattia@gmail.com>
2023-05-06 16:48:29 +0100

    wording cleanups

--- responses/embeds/MissingPermissions.ts
+++ responses/embeds/MissingPermissions.ts
@@ -2,6 +2,6 @@ import { EmbedBuilder } from "discord.js";
 import colors from "./../../constants/colors";
 
 export default new EmbedBuilder({
-    title: "Missing permissions!",
+    title: "🛑 Missing permissions!",
     description: "You don't have enough permissions to use this command!",
 }).setColor(colors.red);
--- responses/embeds/UserNotFound.ts
+++ responses/embeds/UserNotFound.ts
@@ -3,5 +3,5 @@ import colors from "./../../constants/colors";
 
 export default new EmbedBuilder({
     title: "Wait... What?",
-    description: `I don't know anyone who calls themselves that. Make sure you give me a valid name!`,
+    description: "I don't know anyone who calls themselves that. Make sure you give me a valid name!\n\nIf you're trying to fetch your own data, make sure to run the command `/osuset` beforehand!",
 }).setColor(colors.orange);
