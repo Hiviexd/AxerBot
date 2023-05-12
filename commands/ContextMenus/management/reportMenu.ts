77d016944e4733c887ee2cd24f85a4912b60f514
Sebola3461 <mixvideostubecontato@gmail.com>
2023-05-12 17:51:33 -0300

    Fix report menu

--- commands/ContextMenus/management/reportMenu.ts
+++ commands/ContextMenus/management/reportMenu.ts
@@ -6,10 +6,12 @@ import {
     GuildMember,
     ApplicationCommandType,
 } from "discord.js";
+import { randomUUID } from "crypto";
 import {
     ContextMenuCommand,
     ContextMenuType,
 } from "../../../models/commands/ContextMenuCommand";
+import { SendReportEmbed } from "../../../responses/report/SendReportEmbed";
 
 export default new ContextMenuCommand<ContextMenuType.Message>()
     .setName("Report Message")
@@ -17,7 +19,37 @@ export default new ContextMenuCommand<ContextMenuType.Message>()
     .setEphemeral(true)
     .setModal(true)
     .setExecuteFunction(async (command) => {
-        const targetAudio = command.targetMessage.attachments.find(
-            (a) => a.contentType
-        );
+        const modal = new ModalBuilder()
+            .setTitle("Report Message")
+            .setCustomId(randomUUID());
+        const reasonField =
+            new ActionRowBuilder<TextInputBuilder>().setComponents(
+                new TextInputBuilder()
+                    .setLabel("Reason")
+                    .setRequired(true)
+                    .setCustomId("reason")
+                    .setStyle(TextInputStyle.Paragraph)
+            );
+
+        modal.addComponents(reasonField);
+
+        await command.showModal(modal);
+
+        const modalContent = await command.awaitModalSubmit({
+            time: 300000, // 5 minutes,
+        });
+
+        await modalContent.deferUpdate();
+
+        const reportReason = modalContent.fields.getTextInputValue("reason");
+
+        if (!command.targetMessage.member || !command.member) return;
+
+        SendReportEmbed({
+            command,
+            reason: reportReason,
+            reportedUser: command.targetMessage.member,
+            reporter: command.member as GuildMember,
+            messageContent: command.targetMessage.content,
+        });
     });
