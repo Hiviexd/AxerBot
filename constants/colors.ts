319556fea79710606d4e723eee5d2399a5d13b2b
Hivie <rayenattia@gmail.com>
2023-07-02 01:35:21 +0100

    more colors

--- constants/colors.ts
+++ constants/colors.ts
@@ -2,8 +2,10 @@ import { ColorResolvable } from "discord.js";
 
 const colors = {
     green: "#1df27d" as ColorResolvable,
+    darkGreen: "#1d8f1d" as ColorResolvable,
     yellow: "#ffc85a" as ColorResolvable,
     red: "#ff5050" as ColorResolvable,
+    darkRed: "#b30000" as ColorResolvable,
     orange: "#ea6112" as ColorResolvable,
     pink: "#f45592" as ColorResolvable,
     blue: "#008cff" as ColorResolvable,
