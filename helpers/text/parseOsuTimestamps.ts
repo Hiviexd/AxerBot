207fb224377b41de0201b1954ade889f8c5908b9
sebola3461 <mixvideostubecontato@gmail.com>
2023-12-25 16:16:07 -0300

    Fix discussion embed mania timestamps

--- helpers/text/parseOsuTimestamps.ts
+++ helpers/text/parseOsuTimestamps.ts
@@ -1,5 +1,5 @@
 export default (rawText: string) => {
-    const TIMESTAMP = /(\d+):(\d{2}):(\d{3})\s*(\(((\d,?)+)\))?/gim;
+    const TIMESTAMP = /(\d+):(\d{2}):(\d{3})\s*(\(((\d+(\|)?,?)+)\))?/gim;
 
     const timestamps = rawText.match(TIMESTAMP);
 
@@ -9,16 +9,22 @@ export default (rawText: string) => {
 
     for (const timestamp of timestamps) {
         const res = TIMESTAMP.exec(timestamp);
-        TIMESTAMP.lastIndex = 0;
-        if (!res) continue;
 
-        let url = `https://axer-url.vercel.app/api/edit?time=${res[1]}:${res[2]}:${res[3]}`;
+        if (res) {
+            TIMESTAMP.lastIndex = 0;
 
-        if (res[4]) url += `-${res[4]}`;
-        url += "";
-
-        parsed = parsed.replace(timestamp, `[${timestamp}](${url})`);
+            parsed = parsed.replace(
+                timestamp,
+                `[${timestamp.trim()}](${encodeURI(
+                    `https://axer-url.vercel.app/api/edit?time=${res[1]}:${res[2]}:${res[3]}${
+                        res[4] ? `${res[4]}` : ""
+                    }`
+                )})`.replace("-", "")
+            );
+        }
     }
+
+    console.log(parsed);
 
     return parsed;
 };
